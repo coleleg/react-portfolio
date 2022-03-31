0a57e30018a1cbda3558f5bcea97a5f7bfaf0e50
coleleg <coleleg@gmail.com>
2022-03-31 02:40:02 -0400

    style about page

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -2,21 +2,34 @@ import React from 'react';
 
 function About() {
     return(
-        <section>
+        <section class='about'>
             <h1>
                 My Story
             </h1>
-            <div class='card'>
-                    <div class='card-content'>
-                        <h2 class='card-title'>
-                            Weather Dashboard
-                        </h2>
-                        <p class='card-body'>
-                        This Weather App allows you to view the Current Weather and 5-Day-Forecast of any city in the world using the OpenWeather API. 
-                        </p>
-                        <a href='https://coleleg.github.io/weather-dashboard/' class='button'>Try it out!</a>
-                    </div>
-                </div>
+            <div class='about-content'>
+                <p>
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me.
+                    This is everything about me. 
+                </p>
+            </div>
         </section>
     )
 }
