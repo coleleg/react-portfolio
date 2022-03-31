4cb6e976b6d88dce9d855df1844c7e1fc1c3348a
coleleg <coleleg@gmail.com>
2022-03-31 02:09:35 -0400

    add 5/6 projects

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -6,9 +6,17 @@ function About() {
             <h1>
                 My Story
             </h1>
-            <div>
-                <p>My Story goes here</p>
-            </div>
+            <div class='card'>
+                    <div class='card-content'>
+                        <h2 class='card-title'>
+                            Weather Dashboard
+                        </h2>
+                        <p class='card-body'>
+                        This Weather App allows you to view the Current Weather and 5-Day-Forecast of any city in the world using the OpenWeather API. 
+                        </p>
+                        <a href='https://coleleg.github.io/weather-dashboard/' class='button'>Try it out!</a>
+                    </div>
+                </div>
         </section>
     )
 }
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -9,24 +9,74 @@ function Projects(props) {
             <container class='card-container'>
                 <div class='card' id='card-1'>
                     <div class='card-content'>
-                        <h2 class='card-title'>
+                        <h3 class='card-title'>
                             Weather Dashboard
-                        </h2>
+                        </h3>
                         <p class='card-body'>
-                        This Weather App allows you to view the Current Weather and 5-Day-Forecast of any city in the world using the OpenWeather API. 
+                        This Weather App allows you to view the Current Weather and 5-Day-Forecast of any city using the OpenWeather API. 
                         </p>
-                        <a href='https://coleleg.github.io/weather-dashboard/' class='button'>Try it out!</a>
+                        <a href='https://coleleg.github.io/weather-dashboard/' class='button'>Stay Dry!</a>
+                        <a href='https://github.com/coleleg/weather-dashboard' class='button'>GitHub</a>
                     </div>
                 </div>
                 <div class='card' id='card-2'>
                     <div class='card-content'>
-                        <h2 class='card-title'>
+                        <h3 class='card-title'>
                             GymCRM
-                        </h2>
+                        </h3>
+                        <p class='card-body'>
+                            This app was created with the purpose of being a cheap and simple alternative to CRMs for small, club-style gym owners.
+                        </p>
+                        <a href='https://protected-waters-34726.herokuapp.com/' class='button'>Make Gains!</a>
+                        <a href='https://github.com/coleleg/gym-crm' class='button'>GitHub</a>
+                    </div>
+                </div>
+                <div class='card' id='card-3'>
+                    <div class='card-content'>
+                        <h3 class='card-title'>
+                            Team Page Generator
+                        </h3>
+                        <p class='card-body'>
+                        This app prompts users with questions about their employees and generates an HTML page displaying individual cards for each employee.
+                        </p>
+                        <a href='https://drive.google.com/file/d/1qlB_rOKy05aTOW_Cvtm2hhgPLZRllKVA/view' class='button'>Demo</a>
+                        <a href='https://github.com/coleleg/team-page-generator' class='button'>GitHub</a>
+                    </div>
+                </div>
+                <div class='card' id='card-4'>
+                    <div class='card-content'>
+                        <h3 class='card-title'>
+                            Password Generator
+                        </h3>
+                        <p class='card-body'>
+                            This is one of my first applications.  Showcases understanding of JavaScript fundamentals.  Simple, yet effective.
+                        </p>
+                        <a href='https://coleleg.github.io/password-generator/' class='button'>Stay Secure</a>
+                        <a href='https://github.com/coleleg/password-generator/' class='button'>GitHub</a>
+                    </div>
+                </div>
+                <div class='card' id='card-5'>
+                    <div class='card-content'>
+                        <h3 class='card-title'>
+                            Note Taker
+                        </h3>
+                        <p class='card-body'>
+                        Take notes with Express! This is a simple application that allows users to create, save and delete notes with ease.
+                        </p>
+                        <a href='https://young-island-76408.herokuapp.com/' class='button'>Take Note!</a>
+                        <a href='https://github.com/coleleg/note-taker' class='button'>GitHub</a>
+                    </div>
+                </div>
+                <div class='card' id='card-6'>
+                    <div class='card-content'>
+                        <h3 class='card-title'>
+                            GymCRM
+                        </h3>
                         <p class='card-body'>
                             This app was created with the purpose of being a cheap and simple alternative to CRMs for small, club-style gym owners.
                         </p>
                         <a href='https://protected-waters-34726.herokuapp.com/' class='button'>Check it out!</a>
+                        <a href='https://github.com/coleleg/gym-crm' class='button'>GitHub</a>
                     </div>
                 </div>
             </container>
