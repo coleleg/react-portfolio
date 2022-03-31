878916f1a2f388c349ff6a1d28e9f7945bfeb231
coleleg <coleleg@gmail.com>
2022-03-31 00:17:38 -0400

    create card animations for projects

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 function Projects(props) {
     return (
-        <section>
+        <section class='projects'>
             <h1>Projects</h1>
-            <div class='card'>
-                <h2 class='card-title'>
-                    Project 1
-                </h2>
-                <p class='card-body'>
-                    This is my card body.  My project is pretty mediocre.  Don't look at it.  It's so bad.  Thanks.
-                </p>
-                <a href='#' class='button'>Learn More</a>
+            <div class='card' id='card-1'>
+                <div class='card-content'>
+                    <h2 class='card-title'>
+                        Weather Dashboard
+                    </h2>
+                    <p class='card-body'>
+                        This is my card body.  My project is pretty mediocre.  Don't look at it.  It's so bad.  Thanks.
+                    </p>
+                    <a href='#' class='button'>Learn More</a>
+                </div>
             </div>
         </section>
     )
